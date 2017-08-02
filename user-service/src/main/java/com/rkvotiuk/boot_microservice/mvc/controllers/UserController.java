edb72dba94535541fe51a7af092e85b49ae534c4
Rob Kovtiuk <robsonheater@gmail.com>
2017-08-02 13:25:59 +0300

    Init service: user service

---
+++ user-service/src/main/java/com/rkvotiuk/boot_microservice/mvc/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.rkvotiuk.boot_microservice.mvc.controllers;
+
+import com.rkvotiuk.boot_microservice.domain.models.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("")
+    public User getUser(@RequestParam(value = "id", defaultValue = "1") int id){
+        return new User(id);
+    }
+
+}
