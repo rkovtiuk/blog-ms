ef7ff6fb7e38a823d26b2f1f75924665ac448ec5
Rob Kovtiuk <robsonheater@gmail.com>
2017-08-02 12:59:44 +0300

    Init service: user dashboard

---
+++ user-dashboard/src/main/java/com/rkovtiuk/boot_microsevices/UserDashboardApplication.java
@@ -0,0 +1,11 @@
+package com.rkovtiuk.boot_microsevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserDashboardApplication.class, args);
+    }
+}
---
+++ user-dashboard/src/main/java/com/rkovtiuk/boot_microsevices/mvc/controller/UserDashboardController.java
@@ -0,0 +1,10 @@
+package com.rkovtiuk.boot_microsevices.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserDashboardController {
+
+
+
+}
