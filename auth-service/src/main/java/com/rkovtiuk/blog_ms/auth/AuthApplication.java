56c5d8ed9efe561f4eaff48f441c11c19fe5bad1
Rob Kovtiuk <robsonheater@gmail.com>
2017-08-10 15:38:14 +0300

    Auth service: ui

--- auth-service/src/main/java/com/rkovtiuk/blog_ms/auth/AuthApplication.java
+++ auth-service/src/main/java/com/rkovtiuk/blog_ms/auth/AuthApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.rkovtiuk.blog_ms.db", "com.rkovtiuk.blog_ms.auth"})
 @EnableAutoConfiguration
+@ComponentScan({"com.rkovtiuk.blog_ms.db", "com.rkovtiuk.blog_ms.auth"})
 @SpringBootApplication
 public class AuthApplication {
     public static void main(String[] args) {
