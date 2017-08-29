33051c9d88b65ba0b65ccdd5ba0f5fdb939f42df
Rob Kovtiuk <robsonheater@gmail.com>
2017-08-29 18:27:10 +0300

    WebUI service: add starter

---
+++ webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/WebUIApplication.java
@@ -0,0 +1,11 @@
+package com.rkovtiuk.blog_ms.webui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebUIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebUIApplication.class);
+    }
+}
