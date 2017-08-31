22aab01ab51bcdab64ddc6267226847c646d8f3f
Rob Kovtiuk <robsonheater@gmail.com>
2017-08-31 18:32:38 +0300

    WebUI service: controllers

---
+++ webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/controllers/WebUIController.java
@@ -0,0 +1,13 @@
+package com.rkovtiuk.blog_ms.webui.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebUIController {
+
+
+
+}
