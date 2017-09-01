9af782b6b0e1502b809d73fb78b6cce6e693bbfe
Rob Kovtiuk <robsonheater@gmail.com>
2017-09-01 18:33:10 +0300

    WebUI service: controllers -> home page

--- webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/controllers/WebUIController.java
+++ webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/controllers/WebUIController.java
@@ -5,9 +5,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static com.rkovtiuk.blog_ms.webui.utils.Pages.HOME_PAGE;
+
 @Controller
 public class WebUIController {
 
-
+    @RequestMapping("/")
+    public String home(ModelMap model){
+        return HOME_PAGE;
+    }
 
 }
