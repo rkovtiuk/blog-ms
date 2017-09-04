b113b14d410f3ec1aec0886480e194a4548d308a
Rob Kovtiuk <robsonheater@gmail.com>
2017-09-04 18:34:46 +0300

    WebUI service: controllers -> notifications page

--- webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/controllers/WebUIController.java
+++ webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/controllers/WebUIController.java
@@ -1,18 +1,25 @@
 package com.rkovtiuk.blog_ms.webui.controllers;
 
+import com.rkovtiuk.blog_ms.webui.utils.Pages;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static com.rkovtiuk.blog_ms.webui.utils.Pages.HOME_PAGE;
+import static com.rkovtiuk.blog_ms.webui.utils.Pages.NOTIFICATIONS_PAGE;
 
 @Controller
 public class WebUIController {
 
     @RequestMapping("/")
-    public String home(ModelMap model){
+    public String home(ModelMap modelMap){
         return HOME_PAGE;
+    }
+
+    @RequestMapping("/")
+    public String notifications(ModelMap modelMap){
+        return NOTIFICATIONS_PAGE;
     }
 
 }
--- webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/utils/Pages.java
+++ webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/utils/Pages.java
@@ -2,4 +2,5 @@ package com.rkovtiuk.blog_ms.webui.utils;
 
 public final class Pages {
     public static String HOME_PAGE = "home";
+    public static String NOTIFICATIONS_PAGE = "notifications";
 }
