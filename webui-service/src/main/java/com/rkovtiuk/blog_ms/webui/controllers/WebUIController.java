35d457c1061a7f97ca056125a297d45e2b6a3a91
Rob Kovtiuk <robsonheater@gmail.com>
2017-09-06 18:39:37 +0300

    WebUI service: controllers -> blog page

--- webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/controllers/WebUIController.java
+++ webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/controllers/WebUIController.java
@@ -1,10 +1,10 @@
 package com.rkovtiuk.blog_ms.webui.controllers;
 
-import com.rkovtiuk.blog_ms.webui.utils.Pages;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static com.rkovtiuk.blog_ms.webui.utils.Pages.*;
 import static com.rkovtiuk.blog_ms.webui.utils.Pages.DASHBOARD_PAGE;
 import static com.rkovtiuk.blog_ms.webui.utils.Pages.HOME_PAGE;
 import static com.rkovtiuk.blog_ms.webui.utils.Pages.NOTIFICATIONS_PAGE;
@@ -25,6 +25,11 @@ public class WebUIController {
     @RequestMapping("/notifications")
     public String notifications(ModelMap modelMap){
         return NOTIFICATIONS_PAGE;
+    }
+
+    @RequestMapping("/blog")
+    public String blog(ModelMap modelMap){
+        return BLOG_PAGE;
     }
 
 }
--- webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/utils/Pages.java
+++ webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/utils/Pages.java
@@ -1,7 +1,8 @@
 package com.rkovtiuk.blog_ms.webui.utils;
 
 public final class Pages {
-    public static String HOME_PAGE = "home";
-    public static String NOTIFICATIONS_PAGE = "notifications";
-    public static String DASHBOARD_PAGE = "dashboard";
+    public static final String HOME_PAGE = "home";
+    public static final String NOTIFICATIONS_PAGE = "notifications";
+    public static final String DASHBOARD_PAGE = "dashboard";
+    public static final String BLOG_PAGE = "blog_page";
 }
