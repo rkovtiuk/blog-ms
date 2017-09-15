f9d842e1dd59da5a7e83c88b46970b1a8271880d
Rob Kovtiuk <robsonheater@gmail.com>
2017-09-15 22:57:19 +0300

    Statistics module: entities setup

---
+++ core-service/src/main/java/com/rkovtiuk/blog_ms/core/domain/enums/StatisticsType.java
@@ -0,0 +1,14 @@
+package com.rkovtiuk.blog_ms.core.domain.enums;
+
+public enum StatisticsType {
+
+    BLOG_CREATE,
+    BLOG_VIEW,
+    BLOG_DELETE,
+    BLOG_LIKE,
+    BLOG_DISLIKE,
+    COMMENT_CREATE,
+    COMMENT_DELETE,
+    COMMENT_LIKE
+
+}
