0f69ada66fc1cb75bcd7c01ff3c9b4e957b09366
Rob Kovtiuk <robsonheater@gmail.com>
2017-09-24 15:41:00 +0300

    Webui module: refactor

--- webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/controllers/WebUIController.java
+++ webui-service/src/main/java/com/rkovtiuk/blog_ms/webui/controllers/WebUIController.java
@@ -5,9 +5,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static com.rkovtiuk.blog_ms.webui.utils.Pages.*;
-import static com.rkovtiuk.blog_ms.webui.utils.Pages.DASHBOARD_PAGE;
-import static com.rkovtiuk.blog_ms.webui.utils.Pages.HOME_PAGE;
-import static com.rkovtiuk.blog_ms.webui.utils.Pages.NOTIFICATIONS_PAGE;
 
 @Controller
 public class WebUIController {
