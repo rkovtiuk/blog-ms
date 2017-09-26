06f53e5de765bc598ca6bfe652ffd42f91ecd595
Rob Kovtiuk <robsonheater@gmail.com>
2017-09-27 01:12:01 +0300

    Statistics module: fix repo settings

--- statistics-service/src/main/scala/com/rkovtiuk/blog_ms/statistics/repository/StatisticsRepository.scala
+++ statistics-service/src/main/scala/com/rkovtiuk/blog_ms/statistics/repository/StatisticsRepository.scala
@@ -3,7 +3,7 @@ package com.rkovtiuk.blog_ms.statistics.repository
 import com.rkovtiuk.blog_ms.statistics.entity.StatisticsEvent
 import org.springframework.data.repository.CrudRepository
 
-trait StatisticsRepository extends CrudRepository[Long, StatisticsEvent]{
+trait StatisticsRepository extends CrudRepository[StatisticsEvent, Long]{
   def findAllByUser(user: Long): Iterable[StatisticsEvent]
   def findAllByComment(user: Long): Iterable[StatisticsEvent]
   def findAllByBlog(user: Long): Iterable[StatisticsEvent]
