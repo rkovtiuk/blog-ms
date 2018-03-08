819bc12da61a2b3ed402311227cecf5f8f189a64
Rob Kovtiuk <robsonheater@gmail.com>
2018-03-08 21:17:37 +0200

    Docker build: blog module

--- auth-service/src/main/java/com/rkovtiuk/blog_ms/auth/AuthApplication.java
+++ auth-service/src/main/java/com/rkovtiuk/blog_ms/auth/AuthApplication.java
@@ -1,13 +1,12 @@
 package com.rkovtiuk.blog_ms.auth;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan({"com.rkovtiuk.blog_ms.core", "com.rkovtiuk.blog_ms.auth"})
+
 @SpringBootApplication
+@ComponentScan({"com.rkovtiuk.blog_ms.core", "com.rkovtiuk.blog_ms.auth"})
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
